9f221396430206c7150983e320d9750e54f9c6ef
bulutluoz <abbulutluoz@gmail.com>
2024-10-04 19:47:30 +0200

    22.gun sonu

---
+++ src/test/java/stepdefinitions/FacebookStepdefinitions.java
@@ -0,0 +1,23 @@
+package stepdefinitions;
+
+import io.cucumber.java.en.*;
+
+public class FacebookStepdefinitions {
+
+    @Then("facebook cookies kabul eder")
+    public void facebook_cookies_kabul_eder() {
+
+    }
+    @Then("facebook'da yeni hesap olustur butonuna basar")
+    public void facebook_da_yeni_hesap_olustur_butonuna_basar() {
+
+    }
+    @When("Faker class'indan bilgilerle facebook kayit formunu doldurur")
+    public void faker_class_indan_bilgilerle_facebook_kayit_formunu_doldurur() {
+
+    }
+    @Then("facebook kayit ol tusuna basar")
+    public void facebook_kayit_ol_tusuna_basar() {
+
+    }
+}
